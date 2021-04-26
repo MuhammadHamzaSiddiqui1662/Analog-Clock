062165270952e011541be1bfc9232e2529a1aeed
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2021-04-26 18:12:50 +0500

    changed some designing

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const minutesHand = document.querySelector('.minutes-hand');
 const secondsHand = document.querySelector('.seconds-hand');
 const millisecondsHand = document.querySelector('.milliseconds-hand');
 let currentTime;
+var sound = new Audio('./audios/tick.mp3');
 
 setInterval(() => {
     currentTime = new Date;
     millisecondsHand.style.transform = 'rotate('+((currentTime.getMilliseconds()/(1000/360))-90)+'deg)';
     secondsHand.style.transform = 'rotate('+((currentTime.getSeconds()*6)-90)+'deg)';
     minutesHand.style.transform = 'rotate('+((currentTime.getMinutes()*6)-90)+'deg)';
-    hoursHand.style.transform = 'rotate('+((currentTime.getHours()*30)-90)+'deg)';
-    console.log(currentTime.getHours(), currentTime.getMinutes(), currentTime.getSeconds(), currentTime.getMilliseconds());
-}, 1);
-
-console.log(hoursHand);
+    hoursHand.style.transform = 'rotate('+((currentTime.getHours()*30)+(currentTime.getMinutes()/2)-90)+'deg)';
+    sound.play();
+}, 10);
